0ecf8a7452c7f457f23d1d52db574e381a764ff9
Ethan Widen <ethan.widen@faulkner.edu>
2018-01-20 01:31:59 +0000

    HW1: Ruby Intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,85 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sumval = 0
+  arr.each do |val|
+    sumval += val
+  end
+  return sumval
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  val = 0
+  val = arr.max(2).reduce(:+) if !arr.empty?
+  return val
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    return false
+  end
+  if arr.length == 1
+    return false
+  end
+  arr.each_with_index.any? do |y,i|
+    arr.each_with_index.any? do |z,j|
+      unless i==j
+          if (y + z == n)
+            return true
+          end
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return /^([^AEIOU\W])+(.)*$/i =~ s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (/^[0-1]+$/ =~ s)
+    dec = s.to_i(2)
+  else 
+    return false
+  end
+  if (dec % 4 == 0)
+    return true
+  end
+  return false
+    
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    throw ArgumentError if isbn.empty?
+    throw ArgumentError if price <= 0
+      
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  def price=(price)
+    @price = price
+  end
+  def isbn
+    return @isbn
+  end
+  def price
+    return @price
+  end
+  def price_as_string
+    return "$#{'%.2f' % price.to_f}"
+  end
 end
